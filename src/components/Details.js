71ddfdf2f75812cfcdea9f4f93036ab2f0c77e75
aderose <sandro.derose28@yahoo.com>
2020-12-04 19:24:12 +0000

    Pass LinkedIn & GitHub links through defaultProps

--- src/components/Details.js
+++ src/components/Details.js
@@ -6,22 +6,20 @@ function Details(props) {
     <div className="d-flex flex-wrap mx-auto">
       <p>{props.description}</p>
       <div className="description-link">
-        <i className="fas fa-mobile-alt"></i>
+        <i className="fas fa-mobile-alt text-secondary"></i>
         {props.phone}
       </div>
       <div className="description-link">
-        <i className="far fa-envelope"></i>
+        <i className="far fa-envelope text-secondary"></i>
         {props.email}
       </div>
       <div className="description-link">
-        <i className="fab fa-linkedin"></i>
-        <a href={`https://linkedin.com/in/${props.linkedin}`}>
-          {props.linkedin}
-        </a>
+        <i className="fab fa-linkedin text-secondary"></i>
+        <a href={props.linkedin.link}>{props.linkedin.name}</a>
       </div>
       <div className="description-link">
-        <i className="fab fa-github"></i>
-        <a href={`https://github.com/${props.github}`}>{props.github}</a>
+        <i className="fab fa-github text-secondary"></i>
+        <a href={props.github.link}>{props.github.name}</a>
       </div>
     </div>
   );
@@ -45,8 +43,14 @@ Details.defaultProps = {
     'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus sit amet tortor eget turpis fringilla malesuada. Cras vulputate vitae sapien sed pellentesque. In at egestas.',
   phone: '+44 7123 456 789',
   email: 'j.smith@gmail.com',
-  linkedin: 'j-smith',
-  github: 'jsmith',
+  linkedin: {
+    name: 'j-smith',
+    link: 'https://linkedin.com/in/j-smith',
+  },
+  github: {
+    name: 'jsmith',
+    link: 'https://github.com/jsmith',
+  },
 };
 
 export default Details;
