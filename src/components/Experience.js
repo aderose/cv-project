1e88628e939229ae2a46f78dcf94f5695e89172e
aderose <sandro.derose28@yahoo.com>
2020-12-04 20:10:21 +0000

    Add example jobs to work experience

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -1,7 +1,31 @@
 import Card from './Card';
+import CardItem from './CardItem';
 
 function Experience(props) {
-  return <Card header="Experience" icon="fas fa-briefcase" body={props.body} />;
+  const jobs = (
+    <div>
+      <CardItem
+        organisation="Magic Co."
+        tenure={{ start: 2010, end: 2020 }}
+        title="Magician"
+        description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+      />
+      <CardItem
+        organisation="Magic Co."
+        tenure={{ start: 2010, end: 2020 }}
+        title="Magician"
+        description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+      />
+      <CardItem
+        organisation="Magic Co."
+        tenure={{ start: 2010, end: 2020 }}
+        title="Magician"
+        description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+      />
+    </div>
+  );
+
+  return <Card header="Experience" icon="fas fa-briefcase" body={jobs} />;
 }
 
 export default Experience;
