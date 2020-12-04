f1242fbbb918a2e7242c0206b4882bb22d8b8041
aderose <sandro.derose28@yahoo.com>
2020-12-04 15:01:45 +0000

    Create basic App layout

--- src/App.js
+++ src/App.js
@@ -1,5 +1,25 @@
-function App() {
-  return <div>Hello World</div>;
+import React from 'react';
+import Title from './components/Title';
+import Details from './components/Details';
+import Experience from './components/Experience';
+import Education from './components/Education';
+import Skills from './components/Skills';
+import Interests from './components/Interests';
+import './styles/app.css';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="container bg-white my-4 rounded-lg shadow py-3">
+        <Title name="John Smith" title="Web Developer" />
+        <Details />
+        <Experience />
+        <Education />
+        <Skills />
+        <Interests />
+      </div>
+    );
+  }
 }
 
 export default App;
