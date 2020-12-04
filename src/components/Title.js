505197a283a58ad6a6322d9cf0215cb415a97685
aderose <sandro.derose28@yahoo.com>
2020-12-04 15:11:26 +0000

    Implement EditButton component

--- src/components/Title.js
+++ src/components/Title.js
@@ -1,17 +1,14 @@
 import React from 'react';
+import EditButton from './EditButton';
 
 function Title(props) {
   return (
     <div className="text-center border-bottom">
       <div className="d-flex justify-content-center align-items-start">
         <h1 className="text-uppercase text-black-75">{props.name}</h1>
-        <button className="edit">
-          <i className="fas fa-edit text-black"></i>
-        </button>
+        <EditButton color="black" />
       </div>
-      <p className="text-muted font-italic text-capitalize">
-        ~ {props.title} ~
-      </p>
+      <p className="text-muted font-italic text-capitalize">{props.title}</p>
     </div>
   );
 }
