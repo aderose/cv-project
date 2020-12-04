ba8956e3fd6b01582118abc0839ccf7518ca4f0d
aderose <sandro.derose28@yahoo.com>
2020-12-04 21:18:16 +0000

    Highlight card when hovering

--- src/components/Card.js
+++ src/components/Card.js
@@ -5,7 +5,7 @@ import '../styles/card.css';
 function Card(props) {
   return (
     <div className="cv-card overflow-hidden my-3 shadow-sm bg-light">
-      <div className="cv-card-header bg-secondary d-flex align-items-center text-white">
+      <div className="cv-card-header d-flex align-items-center text-white">
         <i className={`${props.icon} card-icon`}></i>
         <strong className="text-white mr-auto">{props.header}</strong>
         <EditButton color="white" onClick={props.onClick} />
