18ad4d23e53be96fb0b52495aa1faf06ca8f4120
aderose <sandro.derose28@yahoo.com>
2020-12-04 22:15:32 +0000

    Implement Brick components to highlight Skills & Interests

--- src/components/Interests.js
+++ src/components/Interests.js
@@ -1,7 +1,18 @@
 import Card from './Card';
+import Brick from './Brick';
 
 function Interests(props) {
-  return <Card header="Interests" icon="fas fa-lightbulb" body={props.body} />;
+  const body = (
+    <div className="d-flex flex-wrap justify-content-center">
+      <Brick content="Magic" />
+      <Brick content="Walking" />
+      <Brick content="Running" />
+      <Brick content="Reading" />
+      <Brick content="Swimming" />
+    </div>
+  );
+
+  return <Card header="Interests" icon="fas fa-lightbulb" body={body} />;
 }
 
 export default Interests;
--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,7 +1,25 @@
 import Card from './Card';
+import Brick from './Brick';
 
 function Skills(props) {
-  return <Card header="Skills" icon="fas fa-cogs" body={props.body} />;
+  const body = (
+    <div className="d-flex flex-wrap justify-content-center">
+      <Brick content="Levitation" />
+      <Brick content="Alchemy" />
+      <Brick content="Necromancy" />
+      <Brick content="Spell Casting" />
+      <Brick content="Elemental" />
+      <Brick content="Chaos" />
+      <Brick content="Dark Arts" />
+      <Brick content="Absorption" />
+      <Brick content="Conjuration" />
+      <Brick content="Divination" />
+      <Brick content="Enchantment" />
+      <Brick content="Illusion" />
+    </div>
+  );
+
+  return <Card header="Skills" icon="fas fa-cogs" body={body} />;
 }
 
 export default Skills;
