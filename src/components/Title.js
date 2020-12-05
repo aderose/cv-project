ffb81b61ca269df8e30d5c889e3d490eb221be96
aderose <sandro.derose28@yahoo.com>
2020-12-05 22:07:42 +0000

    Convert Title to class component

--- src/components/Title.js
+++ src/components/Title.js
@@ -1,16 +1,27 @@
+import { render } from '@testing-library/react';
 import React from 'react';
 import EditButton from './EditButton';
 
-function Title(props) {
-  return (
-    <div className="text-center border-bottom">
-      <div className="d-flex justify-content-center align-items-start">
-        <h1 className="text-uppercase text-black-75">{props.name}</h1>
-        <EditButton color="black" />
+class Title extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { ...this.props };
+  }
+
+  render() {
+    return (
+      <div className="text-center border-bottom">
+        <div className="d-flex justify-content-center align-items-start">
+          <h1 className="text-uppercase text-black-75">{this.state.name}</h1>
+          <EditButton color="black" />
+        </div>
+        <p className="text-muted font-italic text-capitalize">
+          {this.state.title}
+        </p>
       </div>
-      <p className="text-muted font-italic text-capitalize">{props.title}</p>
-    </div>
-  );
+    );
+  }
 }
 
 Title.defaultProps = {
