62814b08e754894e05beaf453c691dbec5b3f503
aderose <sandro.derose28@yahoo.com>
2020-12-06 17:40:33 +0000

    Add trash button to brick elements

--- src/components/Brick.js
+++ src/components/Brick.js
@@ -5,6 +5,7 @@ function Brick(props) {
   return (
     <div className="brick rounded-pill px-3 text-white shadow-sm">
       {props.content}
+      <i className="delete-brick fas fa-trash" onClick={props.onClick}></i>
     </div>
   );
 }
