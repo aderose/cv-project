cc0553e4d2c486cf10a07268854c63c2ba791031
aderose <sandro.derose28@yahoo.com>
2020-12-06 16:56:38 +0000

    Implement add skill button

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Card from './Card';
 import Brick from './Brick';
+import Form from './Form';
 import uniqid from 'uniqid';
 
 class Skills extends React.Component {
@@ -8,8 +9,42 @@ class Skills extends React.Component {
     super(props);
 
     this.state = {
+      ...this.props,
       formActive: false,
     };
+
+    this.addBrick = this.addBrick.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
+  }
+
+  getFormInputs(index) {
+    return [
+      {
+        id: 'brick',
+        name: 'Skill',
+        type: 'text',
+        isLabelled: true,
+        value: index === -1 ? '' : this.state.skills[index],
+      },
+      {
+        id: 'add',
+        name: 'add',
+        type: 'submit',
+        isLabelled: false,
+        value: index === -1 ? 'Add' : 'Update',
+      },
+    ];
+  }
+
+  addBrick() {
+    this.setState({ formActive: !this.state.formActive });
+  }
+
+  onSubmit(output) {
+    this.setState({
+      skills: [...this.state.skills, output.brick],
+      formActive: false,
+    });
   }
 
   render() {
@@ -17,10 +52,16 @@ class Skills extends React.Component {
       <Card
         header="Skills"
         actionIcon="fas fa-plus-circle"
+        onClick={this.addBrick}
         icon="fas fa-cogs"
         body={
           <div className="d-flex flex-wrap justify-content-center">
-            {this.props.skills.map((skill) => (
+            <Form
+              inputs={this.getFormInputs(-1)}
+              onSubmit={this.onSubmit}
+              isActive={this.state.formActive}
+            />
+            {this.state.skills.map((skill) => (
               <Brick content={skill} key={uniqid()} />
             ))}
           </div>
