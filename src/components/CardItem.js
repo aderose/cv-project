32039b03ff0f767a062c1b1983853320c19eb415
aderose <sandro.derose28@yahoo.com>
2020-12-09 11:54:10 +0000

    Update class component to functional component using useState hook

--- src/components/CardItem.js
+++ src/components/CardItem.js
@@ -1,55 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Form from './Form';
 import '../styles/cardItem.css';
 
-class CardItem extends React.Component {
-  constructor(props) {
-    super(props);
+function CardItem(props) {
+  const [isformActive, setFormActive] = useState(false);
 
-    this.state = {
-      formActive: false,
-    };
+  const toggleForm = () => setFormActive(!isformActive);
 
-    this.onEdit = this.onEdit.bind(this);
-    this.onSubmit = this.onSubmit.bind(this);
-  }
+  const submitForm = (output) => {
+    props.onSubmit(output);
+    toggleForm();
+  };
 
-  onSubmit(output) {
-    this.props.onSubmit(output);
-    this.setState({ formActive: !this.state.formActive });
-  }
-
-  onEdit() {
-    this.setState({ formActive: !this.state.formActive });
-  }
-
-  render() {
-    return (
-      <div>
-        <Form
-          inputs={this.props.inputs}
-          onSubmit={this.onSubmit}
-          isActive={this.state.formActive}
-          formType="edit"
-        />
-        <div className="card-item d-flex flex-wrap border-bottom mb-2 align-items-center">
-          <h2 className="organisation text-left">{this.props.organisation}</h2>
-          <i className="fas fa-edit mx-2" onClick={this.onEdit}></i>
-          <i
-            className="fas fa-trash mr-auto"
-            onClick={this.props.onClickTrash}
-          ></i>
-          <p className="dates text-right">
-            {this.props.tenure.start} - {this.props.tenure.end}
-          </p>
-          <p className="title w-100 font-italic text-muted">
-            {this.props.title}
-          </p>
-          <p className="w-100">{this.props.description}</p>
-        </div>
+  return (
+    <div>
+      <Form
+        inputs={props.inputs}
+        onSubmit={submitForm}
+        isActive={isformActive}
+        formType="edit"
+      />
+      <div className="card-item d-flex flex-wrap border-bottom mb-2 align-items-center">
+        <h2 className="organisation text-left">{props.organisation}</h2>
+        <i className="fas fa-edit mx-2" onClick={toggleForm}></i>
+        <i className="fas fa-trash mr-auto" onClick={props.onClickTrash}></i>
+        <p className="dates text-right">
+          {props.tenure.start} - {props.tenure.end}
+        </p>
+        <p className="title w-100 font-italic text-muted">{props.title}</p>
+        <p className="w-100">{props.description}</p>
       </div>
-    );
-  }
+    </div>
+  );
 }
 
 export default CardItem;
