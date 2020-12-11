9eddd7bddcd694822c31806527b387b0bd903e64
aderose <sandro.derose28@yahoo.com>
2020-12-11 19:21:39 +0000

    Refactor to use functional component & state hooks

--- src/components/Title.js
+++ src/components/Title.js
@@ -1,74 +1,62 @@
-import React from 'react';
+import React, { useState } from 'react';
 import CardAction from './CardAction';
 import Form from './Form';
 
-class Title extends React.Component {
-  constructor(props) {
-    super(props);
+function Title(props) {
+  const [isFormActive, setFormStatus] = useState(false);
+  const toggleForm = () => setFormStatus(!isFormActive);
+  const [name, setName] = useState(props.name);
+  const [title, setTitle] = useState(props.title);
 
-    this.state = { ...this.props, formActive: false };
+  const getFormInputs = () => [
+    {
+      id: 'name',
+      name: 'Full Name',
+      type: 'text',
+      isLabelled: true,
+      value: name,
+    },
+    {
+      id: 'title',
+      name: 'Job Title',
+      type: 'text',
+      isLabelled: true,
+      value: title,
+    },
+    {
+      id: 'update',
+      name: 'update',
+      type: 'submit',
+      isLabelled: false,
+      value: 'Update',
+    },
+  ];
 
-    this.onClick = this.onClick.bind(this);
-    this.onSubmit = this.onSubmit.bind(this);
-  }
+  const submitForm = (output) => {
+    setName(output.name);
+    setTitle(output.title);
+    toggleForm();
+  };
 
-  getFormInputs() {
-    return [
-      {
-        id: 'name',
-        name: 'Full Name',
-        type: 'text',
-        isLabelled: true,
-        value: this.state.name,
-      },
-      {
-        id: 'title',
-        name: 'Job Title',
-        type: 'text',
-        isLabelled: true,
-        value: this.state.title,
-      },
-      {
-        id: 'update',
-        name: 'update',
-        type: 'submit',
-        isLabelled: false,
-        value: 'Update',
-      },
-    ];
-  }
-
-  onClick() {
-    this.setState({ formActive: !this.state.formActive });
-  }
-
-  onSubmit(output) {
-    this.setState({ ...output, formActive: false });
-  }
-
-  render() {
-    return (
-      <div className="text-center border-bottom">
-        <Form
-          inputs={this.getFormInputs()}
-          onSubmit={this.onSubmit}
-          isActive={this.state.formActive}
-          formType="edit"
+  return (
+    <div className="text-center border-bottom">
+      <Form
+        inputs={getFormInputs()}
+        onSubmit={submitForm}
+        isActive={isFormActive}
+        formType="edit"
+      />
+      <div className="d-flex justify-content-center align-items-start">
+        <h1 className="text-uppercase text-black-75">{name}</h1>
+        <CardAction
+          actionIcon="fas fa-edit"
+          color="black"
+          onClick={toggleForm}
         />
-        <div className="d-flex justify-content-center align-items-start">
-          <h1 className="text-uppercase text-black-75">{this.state.name}</h1>
-          <CardAction
-            actionIcon="fas fa-edit"
-            color="black"
-            onClick={this.onClick}
-          />
-        </div>
-        <p className="text-muted font-italic text-capitalize">
-          {this.state.title}
-        </p>
       </div>
-    );
-  }
+      <p className="text-muted font-italic text-capitalize">{title}</p>
+    </div>
+  );
 }
 
 Title.defaultProps = {
